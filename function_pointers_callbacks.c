3fb2d7f7dc46e2e0ca7500dbf94ae8f7da7508fd
Abul Bashar <abulbashar98116@gmail.com>
2026-06-03 00:17:15 +0200

    Quick sort algoritm with a compare function pointer as fourth argument, that takes two constant void pointers to build comparision logic to sort any kind of an array

--- function_pointers_callbacks.c
+++ function_pointers_callbacks.c
@@ -67,6 +67,7 @@ int main(void){
 
 // 3️⃣ Function callback pointer has now the design to sort arrays based on different ranking mechanism... Example is based on absolute value of integer sorting in ascending and descending orders.
 
+/*
 #include <stdio.h>
 #include <math.h>
 
@@ -97,3 +98,29 @@ int main(void){
     
     return 0;
 }
+*/
+
+
+// 4️⃣ More generic qsort algorithm found stdlib library. Takes two void pointers as arguments and compare using pointer address with typecast.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int compare(const void* a, const void* b){
+    int A = *((int*)a);
+    int B = *((int*)b);
+    return abs(A) - abs(B);
+}
+
+
+
+int main(void){
+    int i;
+    int A[] = {-31, 21, -25, 3, 13, 7};
+    qsort(A,6,sizeof(int),compare);
+    for(i = 0; i < 6; i++) printf("%d ", A[i]);
+    
+    return 0;
+}
+
