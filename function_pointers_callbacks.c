de25ae2ca17bd2ddc06132c4e3a6194d37b0adbb
Abul Bashar <abulbashar98116@gmail.com>
2026-06-02 22:05:30 +0300

    Function pointer as callback for bubble sort that takes two integers in the compare function, get's the address from main as arguement, Compare logic swaps only if the value is > 0

--- function_pointers_callbacks.c
+++ function_pointers_callbacks.c
@@ -1,3 +1,7 @@
+
+// 1️⃣ Bubble sort algorithm in C Based on higher rank logic
+
+/*
 #include <stdio.h>
 
 void BubbleSort(int* A, int n){
@@ -21,5 +25,40 @@ int main(void){
     for(i = 0; i < 6; i++) printf("%d ", A[i]);
 
 
+    return 0;
+}
+*/
+
+// 2️⃣ Bubble sort using function pointer and callback 
+
+// Callback function should compare two integers, should return 1 if first element has higher rank, should return 0 if elements are equal, should return -1 if second element has higher rank.
+
+#include <stdio.h>
+
+int compare(int a, int b){
+    if(a > b) return 1;
+    else return -1;
+}
+
+void BubbleSort(int* A, int n, int (*compare)(int,int)){
+    int i,j,temp;
+
+    for(i =0; i<n; i++){
+        for(j =0; j < n-1;j++){
+            if((compare(A[j],A[j+1])) > 0){
+                temp = A[j];
+                A[j] = A[j+1];
+                A[j+1] = temp;
+             }
+        }
+    }
+}
+
+int main(void){
+    int i;
+    int A[] = {3,2,1,5,4,6};
+    BubbleSort(A,6,compare);
+    for(i = 0; i < 6; i++) printf("%d ", A[i]);
+    
     return 0;
 }
